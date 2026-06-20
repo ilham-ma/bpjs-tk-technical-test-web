6d675798a4cb0290326295c501179471db409281
Ilham Maulana Mubarok <ilhamaulana13@gmail.com>
2026-06-20 19:25:21 +0700

    feat(app): install and setup prime vue

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import "./assets/css/main.css";
 import { router } from "./plugins/router.plugin";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import "primeicons/primeicons.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(PrimeVue, {
+    theme: { preset: Aura },
+  })
+  .mount("#app");
